41992fb63dfd75b6ab80d9dd0e960a5a9bbb4c27
Junikarp <118621835+Junikarp@users.noreply.github.com>
2023-08-15 19:29:35 +0900

    [Silver I] Title: 직사각형, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2527. 직사각형/직사각형.java
+++ 백준/Silver/2527. 직사각형/직사각형.java
@@ -1,32 +1,56 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static class Rectangle{
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        public Rectangle(int x1, int y1, int x2, int y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < 4; i++) {
-			int x1 = sc.nextInt();
-			int y1 = sc.nextInt();
-			int p1 = sc.nextInt();
-			int q1 = sc.nextInt();
-			int x2 = sc.nextInt();
-			int y2 = sc.nextInt();
-			int p2 = sc.nextInt();
-			int q2 = sc.nextInt();
-			
-			if (p1 < x2 || q1 < y2 || p2 < x1 || q2 < y1) {
-				System.out.println("d");
-			}
-			else if ((x1 == p2 && y1 == q2) || (x1 == p2 && q1 == y2) || (p1 == x2 && q1 == y2) || (p1 == x2 && y1 == q2)) {
-				System.out.println("c");
-			}
-			else if (p1 == x2 || q1 == y2|| p2 == x1 || y1 == q2){
-				System.out.println("b");
-			}
-			else {
-				System.out.println("a");
-			}
-		}
-	}
+        int T = 4;
 
+        for(int t = 0; t<T; t++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int f = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+
+            Rectangle r1 = new Rectangle(a,b,c,d);
+            Rectangle r2 = new Rectangle(e,f,g,h);
+
+            if(r1.x2 < r2.x1 || r1.y2 < r2.y1|| r1.x1>r2.x2 || r1.y1>r2.y2) {
+                sb.append("d").append("\n");
+            }
+            else if ((r1.x1 == r2.x2 && r1.y1 == r2.y2) || (r1.x1 == r2.x2 && r1.y2 == r2.y1) || (r1.x2 == r2.x1 && r1.y2 == r2.y1) || (r1.x2 == r2.x1 && r1.y1 == r2.y2)) {
+                sb.append("c").append("\n");
+            }
+            else if (r1.x2 == r2.x1 || r1.y2 == r2.y1 || r2.x2 == r1.x1 || r1.y1 == r2.y2) {
+                sb.append("b").append("\n");
+            }
+            else {
+                sb.append("a").append("\n");
+            }
+
+        }
+            System.out.print(sb);
+    }
 }
