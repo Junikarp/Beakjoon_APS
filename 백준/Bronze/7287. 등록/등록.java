2a08ca219f09cdb2d8d5a60180ef4a290006b38c
Junikarp <118621835+Junikarp@users.noreply.github.com>
2023-08-24 22:53:36 +0900

    [Bronze V] Title: 등록, Time: 88 ms, Memory: 11264 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        System.out.println(310);
+        System.out.println("zieunpk0924");
+
+    }
+}
