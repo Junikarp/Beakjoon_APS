e9e7ce1c222555d76bffae007ead28c959537b84
Junikarp <118621835+Junikarp@users.noreply.github.com>
2023-08-27 00:01:46 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 76 ms, Memory: 11448 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.java
@@ -0,0 +1,23 @@
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static boolean[][] visited;
+    static boolean[] can;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+
+        System.out.println(N*10 + M);
+    }
+
+}
