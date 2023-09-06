90cf6567943f478e24f988da3cfce3323f2a05f9
Junikarp <118621835+Junikarp@users.noreply.github.com>
2023-09-06 21:29:43 +0900

    [Silver II] Title: N과 M (10), Time: 76 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,59 @@
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+    static boolean[] visited;
+    static int[] res;
+    static int N, M, b;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        res = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0, 0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int d, int idx) {
+        if (d == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(res[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = idx; i < N; i++) {
+            if (!visited[i] && arr[i]!=b) {
+                visited[i] = true;
+                res[d] = arr[i];
+                dfs(d + 1, i + 1);
+                visited[i] = false;
+                b = arr[i];
+            }
+        }
+    }
+}
